2a6bc3c45d056ff10dfa7e6543d1d330c8192c23
Programmer-RD-AI <go2ranuga@gmail.com>
2022-08-17 10:03:34 +0530

    Update download.py
    Co-Authored-By: Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>

--- download.py
+++ download.py
@@ -94,9 +94,9 @@ class Download:
         if download_url_data is None:
             self.download_url_data = []
         if labels is None:
-            labels = ["/m/0bt_c3"]
+            labels = ["/m/0k4j"]
         if labels_r is None:
-            labels_r = ["/m/0bt_c3"]
+            labels_r = ["/m/0k4j"]
         if labels_and_imageids is None:
             labels_and_imageids = [
                 "/media/indika/Sync/Programmer-RD-AI/Programming/Projects/Python/Rest-Api/Car-Object-Detection-REST-API/Find-Card/ML/Model/dataset/open_image_raw_data/oidv6-train-annotations-human-imagelabels.csv",
@@ -215,7 +215,7 @@ class Download:
                 )
                 loader_iter.set_description(str(len(labels_and_imageid)))
             labels_and_imageid.sample(frac=1)
-            return labels_and_imageid[:10000]
+            return labels_and_imageid[:25000]
         except Exception as e:
             raise ValueError(
                 f"""
@@ -243,7 +243,7 @@ class Download:
                 bboxs_df = bboxs_df.append(pd.read_csv(self.bboxs[i]))
             bboxs_df.sample(frac=1)
 
-            return bboxs_df[:10000]
+            return bboxs_df[:25000]
         except Exception as e:
             raise ValueError(
                 f"""
@@ -273,7 +273,7 @@ class Download:
             image_urls_df.sample(frac=1)
 
             print("Loaded Image Urls")
-            return image_urls_df[:10000]
+            return image_urls_df[:25000]
         except Exception as e:
             raise ValueError(
                 f"""
