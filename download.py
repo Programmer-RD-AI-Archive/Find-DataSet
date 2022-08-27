d727f1cdb84d337a65178c95e53b1e6b95463436
Programmer-RD-AI <go2ranuga@gmail.com>
2022-08-27 09:19:03 +0530

    Update download.py
    Co-Authored-By: Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>

--- download.py
+++ download.py
@@ -94,9 +94,9 @@ class Download:
         if download_url_data is None:
             self.download_url_data = []
         if labels is None:
-            labels = ["/m/0kbfd"]
+            labels = ["/m/0jjw"]
         if labels_r is None:
-            labels_r = ["/m/0kbfd"]
+            labels_r = ["/m/0jjw"]
         if labels_and_imageids is None:
             labels_and_imageids = [
                 "/media/indika/Sync/Programmer-RD-AI/Programming/Projects/Python/Rest-Api/Car-Object-Detection-REST-API/Find-Card/ML/Model/dataset/open_image_raw_data/oidv6-train-annotations-human-imagelabels.csv",
